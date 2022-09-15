0f989c10ad0093f9b0d1e6e5e916caea51748c57
cam <cameron.a.sax@gmail.com>
2022-09-15 16:24:39 -0400

    commits

---
+++ READEME.md
@@ -0,0 +1,4 @@
+solution for codewars problem 'Sum of odd numbers'.
+uses a function which takes in a number 'n' representing the # of rows.
+row 1 only has 1 odd number (one), row 2 has 2 odd numbers (3 & 5) etc.
+returns the sum of a;; odd numbers in row 'n'.
---
+++ main.js
@@ -0,0 +1,10 @@
+//clever solution if i do say so myself. function takes in a row number and returns the sum of all odd numbers that would be in that row.
+// (row 1 contains number 1, row 2 contains numbers 3 & 5, row 3 contains numbers 7, 9, & 11 etc) 
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+  }
+
+
+console.log(rowSumOddNumbers(3)) // => 27
+console.log(rowSumOddNumbers(9)) // => 729
+console.log(rowSumOddNumbers(21)) // => 9261
